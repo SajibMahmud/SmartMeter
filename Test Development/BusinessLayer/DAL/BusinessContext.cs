e9265d230083b106cc500e9d07bc812835e0aa52
Abu Sayed <sayedcsekuet@gmail.com>
2013-06-05 09:58:10 +0600

    Changing the configuration and context

--- Test Development/BusinessLayer/DAL/BusinessContext.cs
+++ Test Development/BusinessLayer/DAL/BusinessContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,14 +12,9 @@ namespace BusinessLayer.DAL
 {
     public class BusinessContext: DbContext
     {
-        public BusinessContext()
-            : base("name=SmartMeterEntities")
-        {
-        }
-    
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            throw new UnintentionalCodeFirstException();
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
         }
     
         public DbSet<Address> Addresses { get; set; }
